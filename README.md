3d0280cd854f35fda4217926f37f3146c35cebdb
Németh László Csaba <nlcslaci@gmail.com>
2023-12-19 10:48:21 +0100

    Box components added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # boxgenerator
 Python script to generate boxes (mainly to hold decks of cards, board game components, etc.)
+A dimension of the box are x,y,z units(=probably mm)
+The inner dimension of the box is x-2thickness,y-2thickness,z-2thickness units(thickness=>"külső falak vastagsága")
+The inner walls are "belső falak vastagsága" units wide
+"x komponensek" sets the number of parts the x side is going to be divede (unimplemented method)
+"y komponensek" sets the number of parts the y side is going to be divede (unimplemented method)
+If "megnyitás" is selected the program will open the generated stl
+If there is a problem during reading, or the program finds a problem with the parameters an "error window" is going to appear
--- boxgenerator.py
+++ boxgenerator.py
@@ -6,16 +6,25 @@ from tkinter import ttk
 
 #Beállítások
 #megnyit =  #Megnyitja-e a generált fájlt
-x = 15 #A doboz szélessége
-y = 20 #A doboz mélysége
-z = 5 #A doboz magassága
+x = 150 #A doboz szélessége
+y = 200 #A doboz mélysége
+z = 50 #A doboz magassága
 ykomponens = 2 #A komponensek száma az y tengely mentén
 xkomponens = 1 #A komponensek száma az x tengely mentén
-kfal = 0.2 #Külső falak vastagsága
-bfal =  0.1 #Belső falak vastagsága
-    
-# A csak, hogy vscode alatt is ugyanabba a mappába generáljon
+kfal = 2 #Külső falak vastagsága
+bfal =  1 #Belső falak vastagsága
+
+# Csak, hogy vscode alatt is ugyanabba a mappába generáljon
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+def errorwindow(message):
+    #Error window
+    error=tk.Tk()
+    error.title("Error")
+    error.geometry("200x50")
+    errorlabel = tk.Label(error, text="Hibás argumentumok!")
+    errorlabel.pack()
+    error.mainloop()
+
 def boxgenerator():
     #Értékek beolvasása
     try:
@@ -28,19 +37,13 @@ def boxgenerator():
         bfal = float(bfalvalue.get())
         megnyit = megnyit2.get()
     except Exception:
-        #Error window
-        error=tk.Tk()
-        error.title("Error")
-        error.geometry("200x50")
-        errorlabel = tk.Label(error, text="Hibás argumentumok!")
-        errorlabel.pack()
-        error.mainloop()
+        errorwindow("Hibás argumentumok!")
         return
     #Megadott méretű doboz generálása
     points = [[x,y,z], [x,y,0], [x,0,z], [x,0,0], [0,y,z], [0,y,0], [0,0,z], [0,0,0]]
     cloud = trimesh.points.PointCloud(points)
     hull_mesh = cloud.convex_hull
-
+    
     #Doboz belsejének törlése (külső falak megtartásával)
     x2 = x - kfal
     y2 = y - kfal
@@ -49,23 +52,44 @@ def boxgenerator():
     cloud2 = trimesh.points.PointCloud(poinst2)
     hull_mesh2 = cloud2.convex_hull
     hull_mesh = hull_mesh.difference(hull_mesh2)
-    ###Később kelhet
-    #egyesítés
-    #hull_mesh = hull_mesh + hull_mesh2
-
+    xkomponens*((x-2*kfal)/xkomponens+(3*bfal/xkomponens))
+    #Belső falak generálása x mentén
+    for i in range(1,xkomponens):
+        x3 = i*x2/xkomponens
+        y3 = y2
+        z3 = z2
+        bfal2=bfal/2
+        kfal2=kfal/2
+        poinst3 = [[x3+bfal2+kfal2,y3,z3], [x3+bfal2+kfal2,y3,kfal], [x3+bfal2+kfal2,kfal,z3], [x3+bfal2+kfal2,kfal,kfal], [x3-bfal2+kfal2,y3,z3], [x3-bfal2+kfal2,y3,kfal], [x3-bfal2+kfal2,kfal,z3], [x3-bfal2+kfal2,kfal,kfal]]
+        cloud3 = trimesh.points.PointCloud(poinst3)
+        hull_mesh3 = cloud3.convex_hull
+        hull_mesh = hull_mesh + hull_mesh3
+    
+    #Belső falak generálása y mentén
+    for i in range(1,ykomponens):
+        x3 = x2
+        y3 = i*y2/ykomponens
+        z3 = z2
+        bfal2=bfal/2
+        #2 oldalról van fal és ezért még egyszer bele kell számolni, viszont a belső falat is 2x-er kell ezért a felével még tolunk
+        kfal2=kfal/2
+        poinst3 = [[x3,y3+bfal2+kfal/2,z3], [x3,y3+bfal2+kfal/2,kfal], [x3,y3-bfal2+kfal/2,z2], [x3,y3-bfal2+kfal/2,kfal], [kfal,y3+bfal2+kfal/2,z3], [kfal,y3+bfal2+kfal/2,kfal], [kfal,y3-bfal2+kfal/2,z3], [kfal,y3-bfal2+kfal/2,kfal]]
+        cloud3 = trimesh.points.PointCloud(poinst3)
+        hull_mesh3 = cloud3.convex_hull
+        hull_mesh = hull_mesh + hull_mesh3
     # Exportálás és megnyitás
     hull_mesh.export('output.stl')
-    try:
-        if megnyit:
+    if megnyit:
+        try:
             os.startfile('output.stl')
-    except Exception:
-        hull_mesh.show()
+        except Exception:
+            hull_mesh.show()
 #UI függvények
 def program():
     boxgenerator()
 
 #UI beállítása
-root = tk.Tk()
+root = tk.Tk(className="Box generator")
 
 #x érték beállítása
 xshow =  tk.Label(root, text="x=",anchor="e")
@@ -102,27 +126,28 @@ xkomponensvalue = tk.Entry(root)
 xkomponensvalue.insert(0, xkomponens)
 xkomponensvalue.grid(row=4, column=1)
 
-#Külső falak vastagsága beállítása
+#Külső falak vastagságának beállítása
 kfalshow =  tk.Label(root, text="Külső falak vastagsága=",anchor="e")
 kfalshow.grid(row=5, column=0, sticky="e")
 kfalvalue = tk.Entry(root)
 kfalvalue.insert(0, kfal)
 kfalvalue.grid(row=5, column=1)
 
-#Belső falak vastagsága beállítása
+#Belső falak vastagságának beállítása
 bfalshow =  tk.Label(root, text="Belső falak vastagsága=",anchor="e")
 bfalshow.grid(row=6, column=0, sticky="e")
 bfalvalue = tk.Entry(root)
 bfalvalue.insert(0, bfal)
 bfalvalue.grid(row=6, column=1)
 
-#Megnyitás gomb
+#Megnyitás checkbox
 megnyit2= tk.BooleanVar()
 megnyitas = tk.Checkbutton(root,variable=megnyit2,onvalue=True, offvalue=False,anchor="w")
 megnyitas.select()
 megnyitas.grid(row=7, column=1, sticky="w")
 megnyitaslabel = tk.Label(root, text="Megnyitás:",anchor="e")
 megnyitaslabel.grid(row=7, column=0, sticky="e")
+
 #Generálás gomb
 button = tk.Button(root, text="Generate!", command=program)
 button.grid(row=8, column=0, columnspan=2)
