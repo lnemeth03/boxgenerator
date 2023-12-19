b0a6fb5b3d31f4351d3585a8265ff02497706b1f
Németh László Csaba <nlcslaci@gmail.com>
2023-12-19 11:02:37 +0100

    Inner wall height added

--- boxgenerator.py
+++ boxgenerator.py
@@ -9,6 +9,7 @@ from tkinter import ttk
 x = 150 #A doboz szélessége
 y = 200 #A doboz mélysége
 z = 50 #A doboz magassága
+bz =45 #A doboz belső falainak magassága
 ykomponens = 2 #A komponensek száma az y tengely mentén
 xkomponens = 1 #A komponensek száma az x tengely mentén
 kfal = 2 #Külső falak vastagsága
@@ -31,6 +32,7 @@ def boxgenerator():
         x = float(xvalue.get())
         y = float(yvalue.get())
         z = float(zvalue.get())
+        bz = float(bzvalue.get())
         ykomponens = int(ykomponensvalue.get())
         xkomponens = int(xkomponensvalue.get())
         kfal = float(kfalvalue.get())
@@ -57,7 +59,7 @@ def boxgenerator():
     for i in range(1,xkomponens):
         x3 = i*x2/xkomponens
         y3 = y2
-        z3 = z2
+        z3 = bz
         bfal2=bfal/2
         kfal2=kfal/2
         poinst3 = [[x3+bfal2+kfal2,y3,z3], [x3+bfal2+kfal2,y3,kfal], [x3+bfal2+kfal2,kfal,z3], [x3+bfal2+kfal2,kfal,kfal], [x3-bfal2+kfal2,y3,z3], [x3-bfal2+kfal2,y3,kfal], [x3-bfal2+kfal2,kfal,z3], [x3-bfal2+kfal2,kfal,kfal]]
@@ -69,11 +71,11 @@ def boxgenerator():
     for i in range(1,ykomponens):
         x3 = x2
         y3 = i*y2/ykomponens
-        z3 = z2
+        z3 = bz
         bfal2=bfal/2
         #2 oldalról van fal és ezért még egyszer bele kell számolni, viszont a belső falat is 2x-er kell ezért a felével még tolunk
         kfal2=kfal/2
-        poinst3 = [[x3,y3+bfal2+kfal/2,z3], [x3,y3+bfal2+kfal/2,kfal], [x3,y3-bfal2+kfal/2,z2], [x3,y3-bfal2+kfal/2,kfal], [kfal,y3+bfal2+kfal/2,z3], [kfal,y3+bfal2+kfal/2,kfal], [kfal,y3-bfal2+kfal/2,z3], [kfal,y3-bfal2+kfal/2,kfal]]
+        poinst3 = [[x3,y3+bfal2+kfal/2,z3], [x3,y3+bfal2+kfal/2,kfal], [x3,y3-bfal2+kfal/2,z3], [x3,y3-bfal2+kfal/2,kfal], [kfal,y3+bfal2+kfal/2,z3], [kfal,y3+bfal2+kfal/2,kfal], [kfal,y3-bfal2+kfal/2,z3], [kfal,y3-bfal2+kfal/2,kfal]]
         cloud3 = trimesh.points.PointCloud(poinst3)
         hull_mesh3 = cloud3.convex_hull
         hull_mesh = hull_mesh + hull_mesh3
@@ -112,44 +114,51 @@ zvalue = tk.Entry(root)
 zvalue.insert(0, z)
 zvalue.grid(row=2, column=1)
 
+#bz érték beállítása
+bzshow =  tk.Label(root, text="Belső falak magassága=",anchor="e")
+bzshow.grid(row=3, column=0, sticky="e")
+bzvalue = tk.Entry(root)
+bzvalue.insert(0, bz)
+bzvalue.grid(row=3, column=1)
+
 #y komponensek beállítása
 ykomponensshow =  tk.Label(root, text="y komponensek=",anchor="e")
-ykomponensshow.grid(row=3, column=0, sticky="e")
+ykomponensshow.grid(row=4, column=0, sticky="e")
 ykomponensvalue = tk.Entry(root)
 ykomponensvalue.insert(0, ykomponens)
-ykomponensvalue.grid(row=3, column=1)
+ykomponensvalue.grid(row=4, column=1)
 
 #x komponensek beállítása
 xkomponensshow =  tk.Label(root, text="x komponensek=",anchor="e")
-xkomponensshow.grid(row=4, column=0, sticky="e")
+xkomponensshow.grid(row=5, column=0, sticky="e")
 xkomponensvalue = tk.Entry(root)
 xkomponensvalue.insert(0, xkomponens)
-xkomponensvalue.grid(row=4, column=1)
+xkomponensvalue.grid(row=5, column=1)
 
 #Külső falak vastagságának beállítása
 kfalshow =  tk.Label(root, text="Külső falak vastagsága=",anchor="e")
-kfalshow.grid(row=5, column=0, sticky="e")
+kfalshow.grid(row=6, column=0, sticky="e")
 kfalvalue = tk.Entry(root)
 kfalvalue.insert(0, kfal)
-kfalvalue.grid(row=5, column=1)
+kfalvalue.grid(row=6, column=1)
 
 #Belső falak vastagságának beállítása
 bfalshow =  tk.Label(root, text="Belső falak vastagsága=",anchor="e")
-bfalshow.grid(row=6, column=0, sticky="e")
+bfalshow.grid(row=7, column=0, sticky="e")
 bfalvalue = tk.Entry(root)
 bfalvalue.insert(0, bfal)
-bfalvalue.grid(row=6, column=1)
+bfalvalue.grid(row=7, column=1)
 
 #Megnyitás checkbox
 megnyit2= tk.BooleanVar()
 megnyitas = tk.Checkbutton(root,variable=megnyit2,onvalue=True, offvalue=False,anchor="w")
 megnyitas.select()
-megnyitas.grid(row=7, column=1, sticky="w")
+megnyitas.grid(row=8, column=1, sticky="w")
 megnyitaslabel = tk.Label(root, text="Megnyitás:",anchor="e")
-megnyitaslabel.grid(row=7, column=0, sticky="e")
+megnyitaslabel.grid(row=8, column=0, sticky="e")
 
 #Generálás gomb
 button = tk.Button(root, text="Generate!", command=program)
-button.grid(row=8, column=0, columnspan=2)
+button.grid(row=9, column=0, columnspan=2)
 
 root.mainloop()
